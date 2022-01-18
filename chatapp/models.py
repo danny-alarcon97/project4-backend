58fbd8d3f7e7635f215ea649d1c5d9f2807b6448
daniel-alarcon97 <danny.alarcon1997@gmail.com>
2022-01-17 22:46:42 -0500

    models attempt

--- chatapp/models.py
+++ chatapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Chatroom(models.Model):
+    chat_name = models.CharField(max_length=100)
+    post = models.TextField()
+    date_time = models.DateTimeField(auto_now=False)
+
+    def __str__(self):
+        return self.name
+
+
+
