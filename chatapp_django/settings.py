09bde6725be261c140b718810f8b3f9e5b2dbd6a
daniel-alarcon97 <danny.alarcon1997@gmail.com>
2022-01-18 23:31:32 -0500

    adding basic css

--- chatapp_django/settings.py
+++ chatapp_django/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chatapp',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,16 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'chatapp_django.wsgi.application'
+ASGI_APPLICATION = 'chatapp-django.asgi.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        }
+    }
+}
 
 
 # Database
