4a174599ac4a3025da077e4d6aaf550b91e8b588
daniel-alarcon97 <danny.alarcon1997@gmail.com>
2022-01-21 13:50:21 -0500

    routing.py

--- chatapp/consumers.py
+++ chatapp/consumers.py
@@ -6,9 +6,17 @@ from asgrief.sync import sync_to_async
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%' % self.room_name
+        self.room_group_name = 'chat_%s' % self.room_name
 
         await self.channel_layer.group_add(
             self.room_group_name,
-            self.channel_name,
+            self.channel_name
+        )
+
+        await self.accept()
+    
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
         )
