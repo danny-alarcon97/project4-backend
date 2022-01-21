7364d07365b79ff68798fd1a62d1eda691773897
daniel-alarcon97 <danny.alarcon1997@gmail.com>
2022-01-21 13:41:22 -0500

    consumers.py

---
+++ chatapp/consumers.py
@@ -0,0 +1,14 @@
+import json 
+
+from channels.generic.websocket import AsyncWebsocketConsumer 
+from asgrief.sync import sync_to_async
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%' % self.room_name
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name,
+        )
