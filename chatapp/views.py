c00b2d738bde777c4c9e2ad706b8c483b7b5d773
daniel-alarcon97 <danny.alarcon1997@gmail.com>
2022-01-20 23:57:09 -0500

    room page progress

--- chatapp/views.py
+++ chatapp/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'chatapp/index.html')
+
+def room(request, room_name):
+    username = request.GET.get('username', 'Anonymous')
+
+    return render(request, 'chatapp/room.html', {'room_name': room_name, 'username': username})
